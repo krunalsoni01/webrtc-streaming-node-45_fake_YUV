4dc18f983e8d8332c521d7265c039c9e0384ffbe
Krunal Soni <kgsoni.ec@gmail.com>
2016-03-25 09:04:39 -0400

    third_party removed

--- webrtc-native/third_party/webrtc/src/third_party/websocketpp/obj/local/armeabi/objs/websocketpp/src/sha1/sha1.o.d
+++
@@ -1,5 +0,0 @@
-/home/infosys/b022_fresh/oct30/lib-rtcweb-client/NativeClient/third_party/websocketpp/obj/local/armeabi/objs/websocketpp/src/sha1/sha1.o: \
- /home/infosys/b022_fresh/oct30/lib-rtcweb-client/NativeClient/third_party/websocketpp/src/sha1/sha1.cpp \
- /home/infosys/b022_fresh/oct30/lib-rtcweb-client/NativeClient/third_party/websocketpp/src/sha1/sha1.h
-
-/home/infosys/b022_fresh/oct30/lib-rtcweb-client/NativeClient/third_party/websocketpp/src/sha1/sha1.h:
